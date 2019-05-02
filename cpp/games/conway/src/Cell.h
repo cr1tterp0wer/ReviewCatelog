1d362a59b6ea1d0fd342a6e1f9fb96287685a23f
CHRISTOPHER STENQVIST <christopherstenqvist@CHRISTOPHERs-MacBook-Pro.local>
2019-05-02 03:47:54 -0700

    conway dressing

--- cpp/games/conway/src/Cell.h
+++ cpp/games/conway/src/Cell.h
@@ -10,7 +10,7 @@ class Cell{
   public:
     Cell( int x, int y ){
       pos = new Point( x, y );
-      isAlive = ( ( rand() % 2 ) == 0 ) ? true : false;
+      isAlive = ( ( rand() % 3 ) == 0 ) ? true : false;
     };
 
     Cell( int x, int y, bool a){
@@ -25,6 +25,7 @@ class Cell{
     void despawn(){
       isAlive = false;
     }
+
     int getX(){ return pos->x; }
     int getY(){ return pos->y; }
     bool getIsAlive(){ return isAlive; }
