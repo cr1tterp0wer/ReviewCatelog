d2f089cbed99ed4e30d31f6753c7d1e9c7f0c7d0
cr1tterp0wer <capodacac@gmail.com>
2019-11-05 16:10:32 -0600

    depot: partial menu

--- rails/depot/app/controllers/line_items_controller.rb
+++ rails/depot/app/controllers/line_items_controller.rb
@@ -34,7 +34,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         session[:counter]=0
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to store_index_url, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
